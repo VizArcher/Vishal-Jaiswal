7b3c4080b660eae033542d22dab296518c6a28ad
VizArcher <viztech2002@gmail>
2024-07-27 19:02:16 +0530

    Added Hero, Projects and without CSS skill section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
+
+function App() {
+  console.log('test');
+  return ( 
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+    </>
+  );
+}
+
+export default App;
+ 
