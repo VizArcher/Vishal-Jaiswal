4864405cba7ae3567f7e4475244f45cbc5fc37ba
VizArcher <viztech2002@gmail>
2024-07-28 17:03:01 +0530

    Added Skills, contact and footer section

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,50 @@
+import styles from './ContactStyles.module.css'
+
+function Contact() { 
+  return (
+    <section id='contact' className={styles.container}>
+        <h1 className='sectionTitle'>Contact</h1>
+        <form action=''>
+            <div className='formgroup'>
+                <label htmlFor='name' hidden>
+                    Name
+                </label>
+                <input 
+                    type='text'
+                    name='name'
+                    id='name'
+                    placeholder='Name'
+                    required
+                />
+            </div>
+            <div className='formgroup'>
+                <label htmlFor='email' hidden>
+                    Email
+                </label>
+                <input 
+                    type='text'
+                    name='email'
+                    id='email'
+                    placeholder='Email'
+                    required
+                />
+            </div>
+            <div className='formgroup'>
+                <label htmlFor='message' hidden>
+                    Message
+                </label>
+                <textarea
+                    type='text'
+                    name='message'
+                    id='message'
+                    placeholder='Message'
+                    required>
+                </textarea>
+            </div>
+            <input type='submit' className='hover-btn' value='submit' />
+        </form>
+    </section>
+  )
+}
+
+export default Contact
